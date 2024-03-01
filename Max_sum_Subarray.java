2c81b6f52850f6e0a741b2d679c79dc55eeee296
Kunalanvade <kunalanvade20@gmail.com>
2024-03-01 23:05:37 +0530

    maximum sum of subarray using (prefixSUM)

--- Max_sum_Subarray.java
+++ Max_sum_Subarray.java
@@ -8,17 +8,20 @@ public class Max_sum_Subarray {
         int prefix[] = new int[number.length];
         prefix[0] = number[0];
 
-        for (int i = 1; i < prefix.length; i++) {
+        for (int i = 1; i < prefix.length; i++) 
+        {
             prefix[i] = prefix[i - 1] + number[i];
         }
 
-        for (int i = 0; i < number.length; i++) {
+        for (int i = 0; i < number.length; i++) 
+        {
             int start = i;
 
-            for (int j = i; j < number.length; j++) {
+            for (int j = i; j < number.length; j++) 
+            {
                 int end = j;
-                
-                currSum = start==0 ? prefix[end]:prefix[end] - prefix[start-1];
+
+                currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1];
 
                 if (maxSum < currSum) {
                     maxSum = currSum;
@@ -31,5 +34,6 @@ public class Max_sum_Subarray {
     public static void main(String[] args) {
         int number[] = { 1, -2, 6, -1, 3 };
         Max_Sum_subarray(number);
+
     }
 }
