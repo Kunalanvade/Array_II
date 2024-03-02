b8de4159c58058e3fdf35e95d69eac53a5aa310e
Kunalanvade <kunalanvade20@gmail.com>
2024-03-02 18:00:25 +0530

    maximum sum of sub array using kadanes algorithm

--- Max_sum_Subarray.java
+++ Max_sum_Subarray.java
@@ -1,39 +1,23 @@
 import java.util.*;
 
 public class Max_sum_Subarray {
-
-    public static void Max_Sum_subarray(int number[]) {
-        int currSum = 0;
-        int maxSum = Integer.MIN_VALUE;
-        int prefix[] = new int[number.length];
-        prefix[0] = number[0];
-
-        for (int i = 1; i < prefix.length; i++) 
-        {
-            prefix[i] = prefix[i - 1] + number[i];
-        }
-
-        for (int i = 0; i < number.length; i++) 
-        {
-            int start = i;
-
-            for (int j = i; j < number.length; j++) 
-            {
-                int end = j;
-
-                currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1];
-
-                if (maxSum < currSum) {
-                    maxSum = currSum;
-                }
+    public static void kadanes(int number[]) {
+        int cs = 0;
+        int ms = Integer.MIN_VALUE;
+        for (int i = 0; i < number.length; i++) {
+            cs = cs + number[i];
+            if (cs < 0) {
+                cs = 0;
             }
+            ms = Math.max(cs, ms);
         }
-        System.out.println("max sum : " + maxSum);
+        System.out.println("max sum  of subaray is :" + ms);
+
     }
 
     public static void main(String[] args) {
-        int number[] = { 1, -2, 6, -1, 3 };
-        Max_Sum_subarray(number);
+        int number[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
+        kadanes(number);
 
     }
 }
