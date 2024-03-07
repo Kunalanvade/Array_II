ee53125a6bb3c67fb751e48b02b690e52ccf9a5d
Kunalanvade <kunalanvade20@gmail.com>
2024-03-07 19:53:19 +0530

    trapping water problem

--- Trapped_Water.java
+++ Trapped_Water.java
@@ -1,37 +1,36 @@
-import java.util.*;
-
 public class Trapped_Water {
-    public static void trapped_water(int height[]) {
-
-        // calculating left max boundary
+    public static int trapping(int height[]) {
         int n = height.length;
-        int leftMax[] = new int[height.length];
-        leftMax[0] = height[0];
+
+        // calculate leftarray
+        int leftmax[] = new int[n];
+        leftmax[0] = height[0];
 
         for (int i = 1; i < n; i++) {
-            leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+            leftmax[i] = Math.max(height[i], leftmax[i - 1]);
         }
 
-        // calculating right max boundary
-        int rightMax[] = new int[n];
-        rightMax[n - 1] = height[n - 1];
+        // calculate rightarray
+
+        int rightmax[] = new int[n];
+        rightmax[n - 1] = height[n - 1];
+
         for (int i = n - 2; i >= 0; i--) {
-            rightMax[i] = Math.max(height[i], rightMax[i + 1]);
+            rightmax[i] = Math.max(height[i], rightmax[i + 1]);
         }
-        // main loop
-        for (int i = 0; i < n; i++) {
-            // water level =min(lefmax bound, right max bound)
-            int water_level = Math.min(leftMax[i], rightMax[i]);
-            // trapped water =water level-height[i]
-            int trapped_Water = trapped_Water + water_level - height[i];
-        }
-        return trapped_Water;
 
+        // loop
+        int trooping = 0;
+        for (int i = 0; i < n; i++) {
+            int wt = Math.min(leftmax[i], rightmax[i]);
+            trooping = trooping + (wt - height[i]);
+        }
+        return trooping;
     }
 
     public static void main(String[] args) {
-
-        int height[]= {4,2,0,6,3,2,5};
-        trapped_water();
+        int height[] = { 4, 2, 0, 6, 3, 2, 5 };
+        int water = trapping(height);
+        System.out.println(water);
     }
 }
